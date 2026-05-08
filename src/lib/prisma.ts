68c810158c7f1f771ce337e3f7727446d6b362a8
txrunishere <tsoni9286@gmail.com>
2026-05-08 20:30:03 +0530

    initialize prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,8 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+import { DATABASE_URL } from "../config/env.js";
+
+const adapter = new PrismaPg({ connectionString: `${DATABASE_URL}` });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
